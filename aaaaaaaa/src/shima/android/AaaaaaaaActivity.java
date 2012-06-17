0e840d023fe7f78d55c40a7eac18d2671e5f8ca9
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-17 14:10:30 +0900

    aaaa Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

---
+++ aaaaaaaa/src/shima/android/AaaaaaaaActivity.java
@@ -0,0 +1,15 @@
+package shima.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AaaaaaaaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    // テスト　これは　UTF-８
+    // githubではどのように表示されるのかを確認したい。
+}
